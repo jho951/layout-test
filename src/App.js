4d97c7a3f8f1d9484956dbc0cccc04c0b4bec1c1
jho951 <jho951@naver.com>
2024-12-14 12:56:44 +0900

    [CREATE] setting css layout

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+      </header>
+    </main>
+  );
+}
+
+export default App;
