2f385f4facc763ea18cef17a494dde3c5b0e643f
jho951 <jho951@naver.com>
2025-01-06 23:43:31 +0900

    [FEAT] home;

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -5,10 +5,9 @@ import "./Home.css";
 export default function Home() {
   return (
     <Layout>
-      <div className='home cc1'>sdfsdf</div>
-      <div className='home cc2'>sdfsdf</div>
-      <div className='home cc3'>sdfsdf</div>
-      <div className='home cc4'>sdfsdf</div>
+      <div className='home cc1'>Resume</div>
+      <div className='home cc2'>About Me</div>
+      <div className='home cc3'>Category</div>
     </Layout>
   );
 }
